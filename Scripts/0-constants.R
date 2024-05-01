8456f216dd2127e28e7c4a6285f515a9e5f88538
Carina Firkowski <carina.firkowski@apexrms.com>
2024-05-01 08:27:36 -0400

    Add functions

--- Scripts/0-constants.R
+++ Scripts/0-constants.R
@@ -33,3 +33,43 @@ spatialDataDir <- file.path(dataDir, "Spatial")
 libraryInputsDir <- file.path(libraryDir, "Inputs", "stconnect v1.1.20")
 
 
+
+# Functions --------------------------------------------------------------------
+
+# Create or update sub-scenarios
+edit_subscenario <- function(scenarioName, scenarioInput, scenarioDatasheet){
+  
+  # Create a scenario
+  newScenario <- scenario(
+    ssimObject = stconnectProject,
+    scenario = scenarioName)
+  
+  # Load input file
+  inputDS <- read.csv(file.path(libraryInputsDir, scenarioInput))
+  
+  # Save datasheet
+  saveDatasheet(ssimObject = newScenario, 
+                data = inputDS, 
+                name = scenarioDatasheet)
+  
+  return(newScenario)
+}
+
+# Create scenario and set dependencies
+create_fullscenario <- function(scenarioName, 
+                                destinationFolder = NA,
+                                dependenciesCore, dependenciesUnique){
+  
+  # Create scenario
+  newScenario <- scenario(ssimObject = stconnectProject,
+                          scenario = scenarioName)
+  # Move scenario into folder
+  if(suppressWarnings(!is.na(destinationFolder))){
+    folderId(newScenario) <- folderId(destinationFolder)
+  }
+  # Set dependencies
+  dependency(newScenario, dependency = c(dependenciesCore, 
+                                         dependenciesUnique))
+  
+}
+
